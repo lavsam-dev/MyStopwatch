d6e1a89159bd3be914e6c5751e9d946fe39800e9
lavsam-dev <lavsam@gmail.com>
2021-12-09 21:54:38 +0400

    Initial commit

---
+++ app/src/main/java/lavsam/gb/mystopwatch/MainActivity.kt
@@ -0,0 +1,11 @@
+package lavsam.gb.mystopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
